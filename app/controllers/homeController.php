70625b89335a0818e523b79de117dd88c62c7bc2
tuanhien72 <tuanhien722003@gmail.com>
2026-05-15 08:51:52 +0700

    URL Process

---
+++ app/controllers/homeController.php
@@ -0,0 +1,11 @@
+<?php
+class homeController() {
+    public function index() {
+        echo "Đây là trang chủ";
+    }
+
+    public function about() {
+        echo "Đây là trang giới thiệu";
+    }
+}
+?>
---
+++ app/controllers/sinhvienController.php
@@ -0,0 +1,11 @@
+<?php
+class sinhvienController() {
+    public function index() {
+        echo "Đây là danh sách sinh viên";
+    }
+
+    public function create() {
+        echo "Đây là trang tạo sinh viên";
+    }
+}
+?>
