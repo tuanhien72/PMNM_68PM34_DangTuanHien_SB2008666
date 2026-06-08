740687019021d022a66bc43c3d50bac64b3027a0
tuanhien72 <tuanhien722003@gmail.com>
2026-06-08 16:34:07 +0700

    feat <UI>: add layout master, partial

--- app/views/sinhvien/create.php
+++ app/views/sinhvien/create.php
@@ -7,5 +7,14 @@
 </head>
 <body>
     <h2>Thêm sinh viên</h2>
+    <form action="/sinhvien/store" method = "POST">
+        <label for="hoten">Họ tên</label>
+        <input type="text" name="hoten" id="hoten">
+        <label for="gioitinh">Giới tính</label>
+        <input type="text" name="gioitinh" id="gioitinh">
+        <label for="mssv">MSSV</label>
+        <input type="text" name="mssv" id="mssv">
+        <input type="submit" class="submit" value="Thêm mới">
+    </form>
 </body>
 </html>
